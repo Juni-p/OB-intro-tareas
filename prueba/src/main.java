8b751b039b96095c16c39f4c54bf43b20bf1afa6
Junior <vitteri81@gmail.com>
2022-05-19 02:54:36 -0300

    Ejemplo funciones en Java.

---
+++ prueba/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args){
+        int resultado;
+        resultado = suma(10,10);
+        System.out.println(resultado);
+    }
+
+    public static int suma(int a, int b){
+        return a + b;
+    }
+}
